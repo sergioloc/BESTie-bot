56505f27d54166de43ef0e19599e919370e2f00a
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-14 14:12:08 +0200

    Fix gitignore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const {token} = require('./config.json');
 
 client.on("ready", () => {
     console.log("Estoy listo!");
@@ -12,4 +13,4 @@ client.on("ready", () => {
  
  });
  
- client.login("TokenSecreto");
+ client.login(token);
