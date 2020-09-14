f89f8c60cb6fc45c9463f343eec78f6fa1684ba7
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-14 15:49:18 +0200

    Prepare for hosting

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const {token} = require('./config.json');
 
+function presence(){
+  client.user.setPresence({
+      status: "online", //dnd, idle, invisible, online
+      activity: {
+          name: "Cantus",
+          type: "PLAYING" //LISTENING, PLAYING, STREAMING, WATCHING
+      }
+  })
+}
+
 client.on("ready", () => {
     console.log("Estoy listo!");
+    presence();
  });
  
  client.on("message", (message) => {
