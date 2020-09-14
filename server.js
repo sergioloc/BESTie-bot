41c7294a626147606a9c5c083eb41e27375e9b15
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-14 14:00:09 +0200

    Upload empty project

---
+++ server.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("Estoy listo!");
+ });
+ 
+ client.on("message", (message) => {
+   if(message.content.startsWith("ping")) {
+     message.channel.send("pong!");
+   }
+ 
+ });
+ 
+ client.login("TokenSecreto");
