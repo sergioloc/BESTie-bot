918e25dcfa86a4e22904d862d23f2487836ceba9
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-15 11:01:55 +0200

    Set simple command

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 //const {token} = require('./config.json');
+const prefix = '-'
 
 function presence(){
   client.user.setPresence({
@@ -19,12 +20,19 @@ client.on("ready", () => {
  });
  
 client.on("message", (message) => {
-  if(message.content.startsWith("ping")) {
-    message.channel.send("Movido a teast");
-    const mem = message.mentions.members.first()
-    const chan = client.channels.get('test')
-    mem.setVoiceChannel(chan)
+  if(!!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping'){
+    message.channel.send('pong');
   }
+    //message.channel.send("Movido a test");
+    //const mem = message.mentions.members.first()
+    //const chan = client.channels.get('test')
+    //mem.setVoiceChannel(chan)
+  
 });
 
 client.login(process.env.BOT_TOKEN);
