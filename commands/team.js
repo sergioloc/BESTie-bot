7896b83b1a3fd64e5d49ddc3f3ca555b2534a5ee
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-15 21:32:57 +0200

    Delete star from role

--- commands/team.js
+++ commands/team.js
@@ -3,14 +3,13 @@ module.exports = {
     description: 'Move team to their channel',
 
     execute(message, args){
-        roleArgument = args[0]
-        channelArgument = args[0]
+        argument = args[0]
         for (var i = 1; i < args.length; i++) {
-            roleArgument = roleArgument + ' ' + args[i];
-            channelArgument = channelArgument + args[i];
+            if (!args[i].includes('★'))
+                argument = argument + ' ' + args[i];
         }
-        const role = message.guild.roles.cache.find(r => r.name == `${roleArgument}`).members.map(m=>m);
-        const channel = message.guild.channels.cache.find(channel => channel.name == `${channelArgument}`);
+        const role = message.guild.roles.cache.find(r => r.name == `${argument}`).members.map(m=>m);
+        const channel = message.guild.channels.cache.find(channel => channel.name == `${argument}`);
         for (var i = 0; i < role.length; i++) {
             role[i].voice.setChannel(`${channel.id}`);
         }
