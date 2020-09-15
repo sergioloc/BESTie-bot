c0111d27b67640bfc42cf75de370dc07a4614521
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-15 21:23:04 +0200

    Fix syntax error

--- commands/team.js
+++ commands/team.js
@@ -9,10 +9,10 @@ module.exports = {
             roleArgument = roleArgument + ' ' + args[i];
             channelArgument = channelArgument + args[i];
         }
-        const everyone = message.guild.roles.cache.find(r => r.name == `${argument}`).members.map(m=>m);
-        const channel = message.guild.channels.cache.find(channel => channel.name == `${argument}`);
-        for (var i = 0; i < everyone.length; i++) {
-            everyone[i].voice.setChannel(`${channel.id}`);
+        const role = message.guild.roles.cache.find(r => r.name == `${roleArgument}`).members.map(m=>m);
+        const channel = message.guild.channels.cache.find(channel => channel.name == `${channelArgument}`);
+        for (var i = 0; i < role.length; i++) {
+            role[i].voice.setChannel(`${channel.id}`);
         }
     }
 }
