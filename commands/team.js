6bf8e6cced1a119e5ae4a24adbc2ff0a038bc2b4
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-15 21:55:20 +0200

    Add stars to role

--- commands/team.js
+++ commands/team.js
@@ -5,10 +5,41 @@ module.exports = {
     execute(message, args){
         argument = args[0]
         for (var i = 1; i < args.length; i++) {
-            if (!args[i].includes('★'))
-                argument = argument + ' ' + args[i];
+            argument = argument + ' ' + args[i];
         }
-        const role = message.guild.roles.cache.find(r => r.name == `${argument}`).members.map(m=>m);
+        role = undefined
+        i = 0
+        while (role == undefined && i <= 7){
+            stars = '';
+            switch (i){
+                case 0:
+                    break; 
+                case 1:
+                    stars = '★ ';
+                    break; 
+                case 2:
+                    stars = '★★ ';
+                    break;
+                case 3:
+                    stars = '★★★ ';
+                    break;
+                case 4:
+                    stars = '★★★★ ';
+                    break;
+                case 5:
+                    stars = '★★★★★ ';
+                    break;
+                case 5:
+                    stars = '★★★★★★ ';
+                    break;
+                case 5:
+                    stars = '★★★★★★★ ';
+                    break;
+            }
+            role = message.guild.roles.cache.find(r => r.name == `${stars + argument}`)
+            i++;
+        } 
+        role.members.map(m=>m);
         const channel = message.guild.channels.cache.find(channel => channel.name == `${argument}`);
         for (var i = 0; i < role.length; i++) {
             role[i].voice.setChannel(`${channel.id}`);
