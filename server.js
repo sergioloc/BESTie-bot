ec965012572c8b9accda534b2ec2146b751203b8
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-15 11:55:47 +0200

    New join command

--- server.js
+++ server.js
@@ -28,28 +28,86 @@ client.on("message", (message) => {
   if (command == 'ping'){
     message.channel.send('pong');
   }
+
   else if (command == 'move'){
-    checkPermissions()
-
-    //Set channels
     alias = client.guild.config.alias;
-    newChannel = message.member.voice.channel;
-    oldChannelId = "";
-    oldChannelName = "test";
 
+    if (message.webhookID) {
+      return client.lib.message.send(client, message.channel, "CANNOT_WEBHOOK");
+    }
+
+    oldChannel = message.member.voice.channel;
+    var newChannel;
+
+    //Check if no argument is passed
+    if (typeof args[0] == "undefined") {
+      return client.lib.message.send(
+        client,
+        message.channel,
+        "PROVIDE_CHANNELNAME"
+      );
+    }
+
+    //Check if the user is part of a voicechannel.
+    if (typeof oldChannel === "undefined") {
+      return client.lib.message.send(
+        client,
+        message.channel,
+        "NOT_IN_VOICECHANNEL"
+      );
+    }
+
+    //Tried to find the channelID from the name
     for (var key in alias) {
-      if (alias[key].includes(oldChannelName)) {
-        oldChannelId = key;
+      if (alias[key].includes(args[0])) {
+        //newChannelId = key;
+        newChannel = message.guild.channels.cache.find((val) => val.id === key);
         break;
       }
     }
 
-    oldChannel = message.guild.channels.cache.find(
-      (val) => val.id === oldChannelId
+    //Check if no key is found
+    if (typeof newChannel === "undefined") {
+      return client.lib.message.send(client, message.channel, "NO_SUCH_CHANNEL", {
+        newChannelName: args[0],
+      });
+    }
+
+    //Check if it is the same channel.
+    if (oldChannel.id == newChannel.id) {
+      return client.lib.message.send(
+        client,
+        message.channel,
+        "ALREADY_IN_CHANNEL"
+      );
+    }
+
+    //Check bot permissions for the channel
+    if (!client.lib.checkPermissions(client, oldChannel)) {
+      return client.lib.message.send(
+        client,
+        message.channel,
+        "BOT_NO_PERMISSION",
+        { oldChannelName: oldChannel.name }
+      );
+    }
+
+    //Check if the user have permission for the channel.
+    if (!newChannel.memberPermissions(message.member).has("CONNECT")) {
+      return client.lib.message.send(
+        client,
+        message.channel,
+        "USER_NO_PERMISSION",
+        { newChannelName: newChannel.name }
+      );
+    }
+
+    var counter = await client.lib.move.channel(
+      client,
+      message.member,
+      newChannel
     );
 
-    /*Moving users
-    var counter = await client.lib.move.channel(client, oldChannel, newChannel);
     if (!counter) {
       message.channel.send(
         "Could not move to " + newChannel.name + ", is it full?"
@@ -59,20 +117,8 @@ client.on("message", (message) => {
         message: "Could not move to " + newChannel.name + ", is it full?",
       };
     }
-    */
-
   }
   
 });
-
-function checkPermissions(){
-  if (!message.member.hasPermission("MOVE_MEMBERS")) {
-    message.channel.send("You do not have the correct permissions!");
-    
-  }
-  else{
-    message.channel.send("You have the correct permissions.");
-  }
-}
 
 client.login(process.env.BOT_TOKEN);
