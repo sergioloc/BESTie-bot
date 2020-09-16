3e683f03bc36896c51f46120d3dfe1e5c2fa6ccf
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-16 12:00:08 +0200

    Print result for 'all' command

--- commands/all.js
+++ commands/all.js
@@ -3,14 +3,35 @@ module.exports = {
     description: 'Move everyone to same channel',
 
     execute(message, args){
+
+        //Get argument
         argument = args[0]
         for (var i = 1; i < args.length; i++) {
             argument = argument + ' ' + args[i];
         }
+
+        //Get role and channel
         const everyone = message.guild.roles.everyone.members.map(m=>m);
         const channel = message.guild.channels.cache.find(channel => channel.name == `${argument}`);
+
+        //Move members
+        count = 0
         for (var i = 0; i < everyone.length; i++) {
-            everyone[i].voice.setChannel(`${channel.id}`);
+            if (everyone[i].voice.channel != undefined){
+                everyone[i].voice.setChannel(`${channel.id}`);
+                count++;
+            }
+        }
+
+        //Print result
+        if (count == 0){
+            message.channel.send('No one is connected to a voice channel')
+        }
+        else if (count == 1){
+            message.channel.send(`${count} member have been moved to ${argument}`)
+        }
+        else {
+            message.channel.send(`${count} members have been moved to ${argument}`)
         }
     }
 }
