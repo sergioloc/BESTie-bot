50cd1808cfecff94afff55d2ae5788aba6159590
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-16 11:27:56 +0200

    Move multiple teams

--- commands/team.js
+++ commands/team.js
@@ -2,47 +2,11 @@ module.exports = {
     name: 'team',
     description: 'Move team to their channel',
 
-    execute(message, args){
+    execute(client, message, args){
         argument = args[0]
         for (var i = 1; i < args.length; i++) {
             argument = argument + ' ' + args[i];
         }
-        role = undefined
-        i = 0
-        while (role == undefined && i <= 7){
-            stars = '';
-            switch (i){
-                case 0:
-                    break; 
-                case 1:
-                    stars = '★ ';
-                    break; 
-                case 2:
-                    stars = '★★ ';
-                    break;
-                case 3:
-                    stars = '★★★ ';
-                    break;
-                case 4:
-                    stars = '★★★★ ';
-                    break;
-                case 5:
-                    stars = '★★★★★ ';
-                    break;
-                case 5:
-                    stars = '★★★★★★ ';
-                    break;
-                case 5:
-                    stars = '★★★★★★★ ';
-                    break;
-            }
-            role = message.guild.roles.cache.find(r => r.name == `${stars + argument}`)
-            i++;
-        } 
-        roleMembers = role.members.map(m=>m);
-        const channel = message.guild.channels.cache.find(channel => channel.name == `${argument}`);
-        for (var i = 0; i < roleMembers.length; i++) {
-            roleMembers[i].voice.setChannel(`${channel.id}`);
-        }
+        client.commands.get('move').execute(message, argument);
     }
 }
--- commands/teams.js
+++ commands/teams.js
@@ -2,14 +2,14 @@ module.exports = {
     name: 'teams',
     description: 'Move teams to their channel',
 
-    execute(message, args){
+    execute(client, message, args){
         argument = args[0]
         for (var i = 1; i < args.length; i++) {
             argument = argument + ' ' + args[i];
         }
         teams = argument.split(', ');
-        for (var i = 1; i < args.length; i++) {
-            console.log(`${teams[i]}`);
+        for (var i = 0; i < teams.length; i++) {
+            client.commands.get('move').execute(message, teams[i]);
         }
     }
 }
