85e430d41e1b0092d12cc219cfed99c0241c18ef
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-17 18:36:11 +0200

    Use return for sending errors

--- commands/move.js
+++ commands/move.js
@@ -35,37 +35,37 @@ module.exports = {
             role = message.guild.roles.cache.find(r => r.name == `${stars + args}`)
             i++;
         }
-        if (role != undefined){
-            roleMembers = role.members.map(m=>m);
-            if (roleMembers.length != 0){
-                const channel = message.guild.channels.cache.find(channel => channel.name == `${args}` && channel.type == 'voice');
-                if (channel != undefined){
-                    count = 0;
-                    for (var i = 0; i < roleMembers.length; i++) {
-                        if (roleMembers[i].voice.channel != undefined){
-                            roleMembers[i].voice.setChannel(`${channel.id}`);
-                            count++;
-                        }
-                    }
-                    //Print result
-                    if (count == 1){
-                        message.channel.send(`${count} member have been moved to ${args}`)
-                    }
-                    else {
-                        message.channel.send(`${count} members have been moved to ${args}`)
-                    }
-                }
-                else{
-                    message.channel.send(`Couldn't find ${args} voice channel`);
-                }
+        
+        //Check if role exist
+        if (role == undefined)
+            return message.channel.send(`${args} role doesn't exist`);
+
+        const channel = message.guild.channels.cache.find(channel => channel.name == `${args}` && channel.type == 'voice');
+        
+        //Check if channel exist
+        if (channel == undefined)
+            return message.channel.send(`Couldn't find ${args} voice channel`);
+
+        roleMembers = role.members.map(m=>m);
+
+        //Check if there are members of the team
+        if (roleMembers.length == 0)
+            return message.channel.send(`There are no members of ${args}`);
+
+        count = 0;
+        for (var i = 0; i < roleMembers.length; i++) {
+            if (roleMembers[i].voice.channel != undefined){
+                roleMembers[i].voice.setChannel(`${channel.id}`);
+                count++;
             }
-            else{
-                message.channel.send(`There are no members of ${args}`);
-            }
-        }
-        else{
-            message.channel.send(`${args} role doesn't exist`);
         }
         
+        //Print result
+        if (count == 1){
+            message.channel.send(`${count} member has been moved to ${args}`)
+        }
+        else {
+            message.channel.send(`${count} members have been moved to ${args}`)
+        }   
     }
 }
