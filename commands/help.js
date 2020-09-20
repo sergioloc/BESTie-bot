79d8853c56cb2758419e79baeb2a7bbea0ef005d
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-20 19:37:58 +0200

    Add all commands to help embed

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,22 @@
 const Discord = require("discord.js");
 module.exports = {
+    name: 'help',
+    description: 'Help command',
+
     execute(message){
         const embed = new Discord.MessageEmbed()
         .setTitle('BESTie help')
-        .setDescription('Move members at the same time!\nThey must be in a voice channel.')
+        .setDescription('Move members at the same time!\n\u200b')
         .setColor(0x0070b8)
-        .addField('!all [Channel]', 'Move everyone to [Channel]')
-        .addField('!team [Team]', 'Move [Team] members to their voice channel')
-        .addField('!teams [Team1], [Team2]...', 'Move multiple teams members to their voice channel');
+        .addField('__MOVE COMMANDS__', '\u200b')
+        .addField('!all [Channel]', '_Move everyone to [Channel]_\n\u200b')
+        .addField('!team [Team]', '_Move [Team] members to their voice channel_\n\u200b')
+        .addField('!team [Team], [Channel]', '_Move [Team] members to [Channel]_\n\u200b')
+        .addField('!teams [Team1], [Team2]...', '_Move multiple teams members to their voice channel_\n\u200b')
+        .addField('__INFO COMMANDS__', '\u200b')
+        .addField('!list', 'List team names\n\u200b')
+        .addField('!members', '_List team names and their members_\n\u200b')
+        .addField('!membersof [Team]', '_List members of [Team]_');
         message.channel.send(embed);
     }
 };
