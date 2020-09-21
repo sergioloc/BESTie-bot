3f0e51181eff8c3d3acf565b2b3d42b47ab9db62
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-21 15:33:25 +0200

    Add punctuation commands to help

--- commands/help.js
+++ commands/help.js
@@ -8,14 +8,20 @@ module.exports = {
         .setTitle('BESTie help')
         .setDescription('Move members at the same time!\n\u200b')
         .setColor(0x0070b8)
+
         .addField('__MOVE COMMANDS__', '\u200b')
-        .addField('!all [Channel]', '_Move everyone to [Channel]_\n\u200b')
-        .addField('!team [Team]', '_Move [Team] members to their voice channel_\n\u200b')
-        .addField('!team [Team], [Channel]', '_Move [Team] members to [Channel]_\n\u200b')
+        .addField('!all [Channel]', '_Move everyone to [Channel]_')
+        .addField('!team [Team]', '_Move [Team] members to their voice channel_')
+        .addField('!team [Team], [Channel]', '_Move [Team] members to [Channel]_')
         .addField('!teams [Team1], [Team2]...', '_Move multiple teams members to their voice channel_\n\u200b')
+
+        .addField('__PUNCTUATION COMMANDS__', '\u200b')
+        .addField('!plus [Team]', '_Add star to [Team]_')
+        .addField('!minus [Team]', '_Remove star from [Team]_\n\u200b')
+
         .addField('__INFO COMMANDS__', '\u200b')
-        .addField('!list', 'List team names\n\u200b')
-        .addField('!members', '_List team names and their members_\n\u200b')
+        .addField('!list', '_List team names_')
+        .addField('!members', '_List team names and their members_')
         .addField('!membersof [Team]', '_List members of [Team]_');
         message.channel.send(embed);
     }
